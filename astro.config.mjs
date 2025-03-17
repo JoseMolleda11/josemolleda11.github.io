31130848cefa5d43b3358678f207f1d988654549
Jose Molleda <jose.molleda.1999@gmail.com>
2025-03-17 15:56:36 -0500

    New changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://josemolleda11.github.io',
-    base: '/PersonalWebsite/',
+    site: 'https://josemolleda11.github.io/PersonalWebsite',
+    base: '/PersonalWebsite',
 });
