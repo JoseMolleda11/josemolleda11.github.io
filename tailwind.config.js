ff9e85dbbe30440d6a48c1d93fb7b9c462ac229b
Jose Molleda <jose.molleda.1999@gmail.com>
2025-04-29 19:55:55 -0500

    New changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     darkMode: 'class', 
     content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"], 
     theme: {
-      extend: {},
+      extend: {
+      },
     },
     plugins: [],
   };
